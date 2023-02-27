2e4610f443beca68ae62dacf20769ca0e4aba284
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-02-27 10:12:05 -0700

    add Ride class which records riders, revenue, and can allow visitors to ride. Also fix visitor class to return spending_money as an integer.

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,13 +1,13 @@
 class Visitor
   attr_reader :name,
               :height,
-              :spending_money,
               :preferences
+  attr_accessor :spending_money
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money
+    @spending_money = spending_money.split("")[1..-1].join.to_i
     @preferences = Array.new
   end
 
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -18,9 +18,9 @@ RSpec.describe Ride do
 
     it "creates attributes from hash" do
       expect(@ride1.name).to eq("Carousel")
-      expect(@ride1.min_height).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
       expect(@ride1.admission_fee).to eq(1)
-      expect(@ride1.exicement).to eq(:gentle)
+      expect(@ride1.excitement).to eq(:gentle)
       expect(@ride1.total_revenue).to eq(0)
     end
   end
@@ -34,12 +34,11 @@ RSpec.describe Ride do
       @visitor1 = Visitor.new('Bruce', 54, '$10')
       @visitor2 = Visitor.new('Tucker', 36, '$5')
       @visitor3 = Visitor.new('Penny', 64, '$15')
-      @visitor1.
       @visitor1.add_preference(:gentle)
       @visitor2.add_preference(:gentle)
-      @ride1.board_rider(visitor1)
-      @ride1.board_rider(visitor2)
-      @ride1.board_rider(visitor1)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
     end
     
     it "can log riders" do
@@ -52,16 +51,15 @@ RSpec.describe Ride do
     it "rejects riders based on both height and preference" do
       @visitor2.add_preference(:thrilling)
       @visitor3.add_preference(:thrilling)
-      @ride3.board_rider(visitor1)
-      @ride3.board_rider(visitor2)
-      @ride3.board_rider(visitor3)
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
 
-      expect(visitor1.spending_money).to eq(8)
-      expect(visitor2.spending_money).to eq(4)
-      expect(visitor3.spending_money).to eq(13)
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
       expect(@ride3.rider_log).to eq({@visitor3=>1})
       expect(@ride3.total_revenue).to eq(2)
-    end
-    
+    end  
   end
 end
